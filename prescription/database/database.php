09051291de2b3bc1c11d769b37255c2eb78b0c89
Thomas Jones <amythyst-eagle@hotmail.co.uk>
2012-01-30 13:04:00 +0000

    updated database file

--- prescription/database/database.php
+++ prescription/database/database.php
@@ -5,7 +5,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_database = "prescription";
+$db_database = "pharmacy";
 
 function mysql_start(){
     $con = mysql_connect($db_host, $db_username, $db_password);
