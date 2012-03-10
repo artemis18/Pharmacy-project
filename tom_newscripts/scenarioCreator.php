d333e8e9ad2edacf199668cbd030619d88cdd5a3
Thomas Jones <amythyst-eagle@hotmail.co.uk>
2012-03-10 22:58:05 +0000

    begin scenario creator layout

---
+++ tom_newscripts/scenarioCreator.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<style>
+	#prescriptionForm{
+	}
+	#patientForm{
+	}
+	</style>
+	<head>
+		<title> Scenario Creator</title>
+		
+	</head>
+	<body>
+	 <div id= "prescriptionform" >
+	 <p>box will go here</p>
+	 </div>
+	 <div id="questions">
+	 </div>
+	 <div id = "patientForm">
+	 </div>
+	</body>
+</html>
